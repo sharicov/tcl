376cdcae7106017de14a753542301806f3d1d3b2
sharicov <sharicov@gmail.com>
2021-04-04 11:02:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-tce-load –wi tc-install-GUI tc-install mc ntfs-3g htop atop iotop wireless-tools firmware-rtlwifi graphics-5.10.3-tinycore64 Xorg-7.7 alsamixergui dropbear parted chromium-browser sshfs samba screen openssl-1.1.1 curl ezremaster Xfbdev flwm flwm_topside aterm  
+tce-load -wi tc-install-GUI tc-install mc ntfs-3g htop atop iotop wireless-tools firmware-rtlwifi graphics-5.10.3-tinycore64 Xorg-7.7 alsamixergui dropbear parted chromium-browser sshfs samba screen openssl-1.1.1 curl ezremaster Xfbdev flwm flwm_topside aterm  
