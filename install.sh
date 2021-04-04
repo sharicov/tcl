20afbae0af69912cb1c9b45edc9bed9c580d835a
sharicov <sharicov@gmail.com>
2021-04-04 11:38:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/ash
 tce-load -wi tc-install-GUI tc-install mc ntfs-3g htop wireless_tools firmware-rtlwifi graphics-5.10.3-tinycore64 Xorg-7.7 alsa alsamixergui dropbear parted chromium-browser sshfs samba screen xfe openssl-1.1.1 curl ezremaster Xfbdev flwm flwm_topside aterm  
-curl -O -k https://distro.ibiblio.org/tinycorelinux/12.x/x86/release/TinyCore-12.0.iso 
+curl -O -k https://distro.ibiblio.org/tinycorelinux/12.x/x86_64/release/TinyCorePure64-12.0.iso
+sudo mkdir /mn/sr0/cde/
+sudo mount -o loop TinyCorePure64-12.0.iso /mnt/sr0/cde/
 cp -u /mnt/sr0/cde/* /tmp/tce/optional/ 
+startx
